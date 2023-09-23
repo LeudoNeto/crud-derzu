03de77dc00710f0af772f0cc36d2ffda87a4bc66
Leudo Neto <leudoneto1234@gmail.com>
2023-09-23 09:27:21 -0300

    crud update

--- crud_cpp/Gerente.h
+++ crud_cpp/Gerente.h
@@ -100,5 +100,125 @@ class Gerente {
 
           return skills;
         }
-        
+
+      Personagem getPersonagemById(int id) {
+        Personagem personagem; // Cria um objeto Personagem vazio
+        // Abre o arquivo "personagens.txt" para leitura
+        ifstream arquivo("personagens.txt");
+
+        // Verifica se o arquivo foi aberto com sucesso
+        if (!arquivo) {
+            cerr << "Erro ao abrir o arquivo." << endl;
+            return personagem;
+        }
+
+        Personagem p; // Cria um objeto Personagem para ler os dados do arquivo
+
+        // Inicia um loop para ler os registros do arquivo
+        while (p.read(arquivo)) {
+            // Verifica se o ID do personagem lido corresponde ao ID fornecido como argumento
+            if (p.id == id) {
+                // Fecha o arquivo após encontrar o personagem
+                arquivo.close();
+                // Retorna o personagem encontrado
+                return p;
+            }
+        }
+
+        // Fecha o arquivo após não encontrar o personagem
+        arquivo.close();
+        // Define o ID como -1 para indicar que o Personagem não foi encontrado
+        personagem.id = -1;
+        // Retorna o objeto Personagem vazio
+        return personagem;
+    }
+
+  bool updatePersonagemById(int id, Personagem newPersonagem) {
+        // Abre o arquivo "personagens.txt" para leitura
+        ifstream inFile("personagens.txt");
+        // Abre um arquivo temporário "temp.txt" para escrita
+        ofstream outFile("temp.txt");
+
+        // Verifica se os arquivos foram abertos com sucesso
+        if (!inFile || !outFile) {
+            cerr << "Erro ao abrir o arquivo." << endl;
+            return false;
+        }
+
+        Personagem p; // Cria um objeto Personagem para ler os dados do arquivo
+        bool personagemEncontrado = false; // Flag para indicar se o personagem foi encontrado
+
+        // Inicia um loop para ler os registros do arquivo
+        while (p.read(inFile)) {
+            // Verifica se o ID do personagem lido corresponde ao ID fornecido como argumento
+            if (p.id == id) {
+                // Escreve os novos dados do personagem no arquivo temporário
+                outFile << newPersonagem.serialize() << endl;
+                // Marca o personagem como encontrado
+                personagemEncontrado = true;
+            } else {
+                // Escreve o personagem original no arquivo temporário
+                outFile << p.serialize() << endl;
+            }
+        }
+
+        // Fecha os arquivos após concluir as operações
+        inFile.close();
+        outFile.close();
+
+        // Remove o arquivo original e renomeia o arquivo temporário para "personagens.txt"
+        if (personagemEncontrado) {
+            remove("personagens.txt");
+            rename("temp.txt", "personagens.txt");
+        } else {
+            // Se o personagem não foi encontrado, apenas remova o arquivo temporário
+            remove("temp.txt");
+        }
+
+        return personagemEncontrado;
+    }
+
+  bool removePersonagemById(int id) {
+        // Abre o arquivo "personagens.txt" para leitura
+        ifstream inFile("personagens.txt");
+        // Abre um arquivo temporário "temp.txt" para escrita
+        ofstream outFile("temp.txt");
+
+        // Verifica se os arquivos foram abertos com sucesso
+        if (!inFile || !outFile) {
+            cerr << "Erro ao abrir o arquivo." << endl;
+            return false;
+        }
+
+        Personagem p; // Cria um objeto Personagem para ler os dados do arquivo
+        bool personagemEncontrado = false; // Flag para indicar se o personagem foi encontrado
+
+        // Inicia um loop para ler os registros do arquivo
+        while (p.read(inFile)) {
+            // Verifica se o ID do personagem lido corresponde ao ID fornecido como argumento
+            if (p.id != id) {
+                // Escreve o personagem original no arquivo temporário, exceto o que deve ser removido
+                outFile << p.serialize() << endl;
+            } else {
+                // Marca o personagem como encontrado
+                personagemEncontrado = true;
+            }
+        }
+
+        // Fecha os arquivos após concluir as operações
+        inFile.close();
+        outFile.close();
+
+        // Remove o arquivo original e renomeia o arquivo temporário para "personagens.txt"
+        if (personagemEncontrado) {
+            remove("personagens.txt");
+            rename("temp.txt", "personagens.txt");
+        } else {
+            // Se o personagem não foi encontrado, apenas remova o arquivo temporário
+            remove("temp.txt");
+        }
+
+        return personagemEncontrado;
+    }
+
 };
--- crud_cpp/createPersonagem.cpp
+++ crud_cpp/createPersonagem.cpp
@@ -12,4 +12,5 @@ int main(int argc, char *argv[]) {
                g.getSkillById(atoi(argv[16])));
 
   g.create(p);
+  cout << 200 << endl;
 }
